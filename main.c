25258c267f97e15e559aceba29d77c8bdd365627
Rustam <hrustbas@gmail.com>
2024-04-17 18:26:10 +0300

    draw some circles

--- main.c
+++ main.c
@@ -1,6 +1,61 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <stdlib.h>
+#include <string.h>
+
+// CONSTS
+#define WIDTH 640
+#define HEIGHT 480
+
+#define SHIFTX (WIDTH / 2)
+#define SHIFTY (HEIGHT / 2)
+
+const char* getfield(char* line, int num)
+{
+    const char* tok;
+    for (tok = strtok(line, ",");
+            tok && *tok;
+            tok = strtok(NULL, ",\n"))
+    {
+        if (!--num)
+            return tok;
+    }
+    return NULL;
+}
 
 int main() {
-    printf("hello world\n");
+    FILE* result = fopen("result.csv", "r");
+
+    InitWindow(WIDTH, HEIGHT, "Main Windows");
+
+    SetTargetFPS(20);
+
+    int i = 1;
+
+    char line[1024];
+    while (!WindowShouldClose() && fgets(line, 1024, result)) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        //while (fgets(line, 1024, result)) {
+            //char* tmp = strdup(line);
+            //printf("Field 3 would be %s\n", getfield(tmp, 1));
+            // NOTE strtok clobbers tmp
+            //free(tmp);
+        //}
+        //printf("%s\n", line);
+        char* tmp = strdup(line);
+        float x1 = atof(getfield(tmp, 2));
+        tmp = strdup(line);
+        float y1 = atof(getfield(tmp, 4));
+
+        printf("%f\t%f\n", x1, y1);
+
+        DrawCircle((i+=4)%WIDTH,HEIGHT/2,10,RED);
+        DrawCircle(x1, y1, 10.0f,GREEN);
+
+
+        EndDrawing();
+    }
+
+    CloseWindow();                // Close window and OpenGL context
 }

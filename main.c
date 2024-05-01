39447b8de9bf2e5e18f2b52d9877248d90f9baa6
Rustam <hrustbas@gmail.com>
2024-05-01 17:24:22 +0300

    remove libraries

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <raylib.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef MATH_H
+#define MATH_H
+#include <math.h>
+#endif
+
+#define SOLVER_RUNGE_KUTTA_IMPLEMENTATION
+#include "c-numerical-methods/solvers/runge-kutta.h"
+
+#define SOLVER_EULER_IMPLEMENTATION
+#include "c-numerical-methods/solvers/simple.h"
 
 // CONSTS
 #define WIDTH 800
@@ -11,67 +18,84 @@
 #define SHIFTY (HEIGHT / 2)
 
 #define RADIUS 5.0f
-#define FPS 60
+#define FPS 30
 
-const char *getfield(char *line, int num) {
-  const char *tok;
-  for (tok = strtok(line, ","); tok && *tok; tok = strtok(NULL, ",\n")) {
-    if (!--num)
-      return tok;
+#define NBODY_IMPLEMENTATION
+#include "initial_conditions.h"
+
+float **normalize(float **X, unsigned COLS, unsigned ROWS) {
+
+  int i, j;
+  float max = 0.0f;
+
+  for (i = 0; i < ROWS; i++) {
+    for (j = 0; j < COLS; j += 4) {
+      float norm = powf(X[i][j], 2) + powf(X[i][j + 2], 2);
+      if (max < norm) {
+        max = norm;
+      }
+    }
   }
-  return NULL;
-}
+
+  for (i = 0; i < ROWS; i++) {
+    for (j = 0; j < COLS; j += 2) {
+      X[i][j] /= sqrtf(max);
+    }
+  }
+  return X;
+};
+
+Color colors[] = {RED, BLUE, GREEN};
 
 int main() {
-  FILE *result = fopen("result.csv", "r");
+  float *t = malloc(sizeof(float) * N);
+  float dt = (T1 - T0) / N;
+  t[0] = T0;
+  for (int i = 1; i < N; i++) {
+    t[i] = t[i - 1] + dt;
+  }
 
+  float **movement =
+      runge_kutta(Nbodies * 4, N, t, dxdt, X0); // TODO: free `movement`
+  movement = normalize(movement, Nbodies * 4, N);
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < Nbodies * 4; j++) {
+      printf("%6.4f\t", movement[i][j]);
+    }
+    printf("\n");
+  }
+
+  int i = 1;
+#if 1
   InitWindow(WIDTH, HEIGHT, "Main Windows");
 
   SetTargetFPS(FPS);
 
-  int i = 1;
+  int j;
 
   char line[1024];
-  while (!WindowShouldClose() && fgets(line, 1024, result)) {
-    // BeginDrawing();
+  while (!WindowShouldClose() && !IsKeyPressed(KEY_Q) && i < N) {
+    BeginDrawing();
     ClearBackground(BLACK);
-    // while (fgets(line, 1024, result)) {
-    // char* tmp = strdup(line);
-    // printf("Field 3 would be %s\n", getfield(tmp, 1));
-    //  NOTE strtok clobbers tmp
-    // free(tmp);
-    //}
-    // printf("%s\n", line);
-    char *tmp = strdup(line);
-    float x1 = atof(getfield(tmp, 2));
-    tmp = strdup(line);
-    float y1 = atof(getfield(tmp, 4));
-    tmp = strdup(line);
-    float x2 = atof(getfield(tmp, 6));
-    tmp = strdup(line);
-    float y2 = atof(getfield(tmp, 8));
-    // tmp = strdup(line);
-    // float x3 = atof(getfield(tmp, 10));
-    // tmp = strdup(line);
-    // float y3 = atof(getfield(tmp, 12));
-    // tmp = strdup(line);
-    // float x4 = atof(getfield(tmp, 14));
-    // tmp = strdup(line);
-    // float y4 = atof(getfield(tmp, 16));
-
     // DrawCircle((i+=4)%WIDTH,HEIGHT/2,10,RED);
-    DrawCircle((int)(SHIFTX * x1) + SHIFTX, (int)(SHIFTY * y1) + SHIFTY, RADIUS,
-               GREEN);
-    DrawCircle((int)(SHIFTX * x2) + SHIFTX, (int)(SHIFTY * y2) + SHIFTY, RADIUS,
-               RED);
-    // DrawCircle((int)(SHIFTX*x3)+SHIFTX, (int)(SHIFTY*y3)+SHIFTY, RADIUS,
-    // BLUE); DrawCircle((int)(SHIFTX*x4)+SHIFTX, (int)(SHIFTY*y4)+SHIFTY,
-    // RADIUS, YELLOW);
 
+    for (j = 0; j < Nbodies * 4; j += 4) {
+      int cx = (movement[i][j] * SHIFTX) + SHIFTX;
+      int cy = (movement[i][j + 2] * SHIFTY) + SHIFTY;
+      DrawCircle(cx, cy, RADIUS, colors[j / 4]);
+    }
+
+    i += 1;
     EndDrawing();
   }
 
-  fclose(result);
-
   CloseWindow(); // Close window and OpenGL context
+#endif
+  for (i = 0; i < N; i += 4) {
+    free(movement[i]);
+    movement[i] = NULL;
+  }
+  free(movement);
+  free(t);
 }

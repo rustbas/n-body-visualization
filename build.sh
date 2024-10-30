34427f6ba4ffb0c42f8f794c3063898c79c36faf
Basyrov Rustam <hrustbas@gmail.com>
2024-10-30 19:49:46 +0300

    fix raylib include

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -xe
 
-gcc ffmpeg_linux.c main.c -ggdb -o result.o -lraylib -lGL -lm
+gcc ffmpeg_linux.c main.c -ggdb -o result.o -lraylib -lGL -lm -I../raylib-5.0_linux_amd64/include -L../raylib-5.0_linux_amd64/lib/libraylib.a
 ./result.o
 

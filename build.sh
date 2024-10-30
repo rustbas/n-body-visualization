fb228ab6fb8494367b050e7733c259f306bd4e2b
Basyrov Rustam <hrustbas@gmail.com>
2024-10-30 20:04:00 +0300

    fix build
    need static link via arguments

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -xe
 
-gcc ffmpeg_linux.c main.c -ggdb -o result.o -lraylib -lGL -lm -I../raylib-5.0_linux_amd64/include -L../raylib-5.0_linux_amd64/lib/libraylib.a
+gcc ffmpeg_linux.c main.c ../raylib-5.0_linux_amd64/lib/libraylib.a -ggdb -o result.o  -lGL -lm -I../raylib-5.0_linux_amd64/include -L../raylib-5.0_linux_amd64/lib/
 ./result.o
 

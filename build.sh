e3902a25349b40a5403fdc87310f1ff37874d96e
Basyrov Rustam <hrustbas@gmail.com>
2024-10-31 15:52:22 +0300

    prettify build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 set -xe
 
-gcc main.c ../raylib-5.0_linux_amd64/lib/libraylib.a -ggdb -o result.o  -lGL -lm -I../raylib-5.0_linux_amd64/include -L../raylib-5.0_linux_amd64/lib/
+gcc main.c ../raylib-5.0_linux_amd64/lib/libraylib.a  \
+  -ggdb -o result.o  -lGL -lm                         \
+  -I../raylib-5.0_linux_amd64/include                 \
+  -L../raylib-5.0_linux_amd64/lib/                    \
+
 ./result.o
 
